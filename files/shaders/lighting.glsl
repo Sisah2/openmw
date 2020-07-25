8271b0b1407e40dd8fdfe0c4548ec9625c8a339d
Sisah <jirielbunda@gmail.com>
2020-07-25 13:53:07 +0200

    move get specular, remove unused shader files

--- files/shaders/lighting.glsl
+++ files/shaders/lighting.glsl
@@ -74,15 +74,3 @@ vec4 doLighting(vec3 viewPos, vec3 viewNormal, vec4 vertexColor)
 #endif
     return lightResult;
 }
-
-
-vec3 getSpecular(vec3 viewNormal, vec3 viewDirection, float shininess, vec3 matSpec)
-{
-    vec3 lightDir = normalize(gl_LightSource[0].position.xyz);
-    float NdotL = dot(viewNormal, lightDir);
-    if (NdotL <= 0.0)
-        return vec3(0.,0.,0.);
-    vec3 halfVec = normalize(lightDir - viewDirection);
-    float NdotH = dot(viewNormal, halfVec);
-    return pow(max(NdotH, 0.0), max(1e-4, shininess)) * gl_LightSource[0].specular.xyz * matSpec;
-}
