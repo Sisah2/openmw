597cb7588225c33e1fe1613dc82d9162ddc46a2b
Sisah2 <64099446+Sisah2@users.noreply.github.com>
2023-12-11 20:05:20 +0100

    Update groundcover.hpp

--- components/settings/categories/groundcover.hpp
+++ components/settings/categories/groundcover.hpp
@@ -16,11 +16,13 @@ namespace Settings
 {
     struct GroundcoverCategory : WithIndex
     {
-        SettingValue<bool> mEnabled{ "Groundcover", "enabled" };
-        SettingValue<bool> mPaging{ "Groundcover", "paging" };
-        SettingValue<bool> mAutoLoad{ "Groundcover", "auto load" };
-        SettingValue<float> mMinChunkSize{ "Groundcover", "min chunk size", makeClampSanitizerFloat(0.125, 1) };
-        SettingValue<float> mMergeFactor{ "Groundcover", "merge factor", makeMaxSanitizerFloat(0) };
+        using WithIndex::WithIndex;
+
+        SettingValue<bool> mEnabled{ mIndex, "Groundcover", "enabled" };
+        SettingValue<bool> mPaging{ mIndex, "Groundcover", "paging" };
+        SettingValue<bool> mAutoLoad{ mIndex, "Groundcover", "auto load" };
+        SettingValue<float> mMinChunkSize{ mIndex, "Groundcover", "min chunk size", makeClampSanitizerFloat(0.125, 1) };
+        SettingValue<float> mMergeFactor{ mIndex, "Groundcover", "merge factor", makeMaxSanitizerFloat(0) };
         SettingValue<float> mDensity{ mIndex, "Groundcover", "density", makeClampSanitizerFloat(0, 1) };
         SettingValue<float> mRenderingDistance{ mIndex, "Groundcover", "rendering distance", makeMaxSanitizerFloat(0) };
         SettingValue<int> mStompMode{ mIndex, "Groundcover", "stomp mode", makeEnumSanitizerInt({ 0, 1, 2 }) };
